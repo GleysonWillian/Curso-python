bdd468cf8ce64f2d02e57ac7bd8d8b8b860e75e2
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2025-01-03 17:56:32 -0300

    Studying

---
+++ input_&_cia.py
@@ -0,0 +1,10 @@
+# input de usuário
+nome = input("Digite o seu nome: ")
+idade = int(input(f"Qual a sua idade, {nome}? "))
+
+nascimento = 2025 - idade
+
+print(f"{nome}, você nasceu em {nascimento}.")
+
+
+# Como fazer comentário? basta colocar a hashtag(#)
