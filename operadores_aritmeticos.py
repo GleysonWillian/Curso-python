6b74957c9940f2dd8eda450f954b046fcf35f1f0
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2025-01-03 15:57:48 -0300

    .

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,18 @@
+num1 = 5
+num2 = 3
+
+soma = (num1 + num2)
+sub = (num1 - num2)
+multi = (num1 * num2)
+divi = (num1 / num2)
+pot = (num1 ** num2)
+res_div = (num1 % num2)
+div_int = (num1 // num2)
+
+print("Resultado da soma:", soma)
+print("Resultado da subtração:", sub)
+print("Resultado da multiplicação:", multi)
+print("Resultado da divisão:", divi)
+print("Resultado da potenciação:", pot)
+print("Resultado do resto da divisão:", res_div)
+print("Resultado da divisão inteira:", div_int)
