c7b0f11b9ac6fab465ed9a2becfa2891b7a6fb40
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2025-01-03 16:16:24 -0300

    Funções: ok.

---
+++ funcoes.py
@@ -0,0 +1,15 @@
+import math
+
+# Funções básicas:
+
+print(abs(-15)) #valor absoluto
+print(pow(3,3)) #função de exponenciação
+print(max(1,5,-55,15,18)) #busca o valor máximo
+print(min(5,1,29,-80)) #busca o valor mínimo 
+print(round(8.8)) #arredondamento (abaixo de .5 vai ser menos e acima de .5 vai ser mais)
+
+#Funções da biblioteca de matemática:
+
+print(math.floor(8.8)) #'floor' arredonda para baixo
+print(math.ceil(8.9999999)) #'ceil' arredonda para cima
+print(math.sqrt(9)) # raiz quadrada
